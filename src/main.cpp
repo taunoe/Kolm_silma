a0911beaf153b0c92a9da7b4b1c8213e1084ca47
Tauno Erik <sedumacre@gmail.com>
2022-03-10 21:24:01 +0200

    +funcs

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@
  *          RCWL-0516 Microwave Proximity sensor
  * Author:  Tauno Erik https://taunoerik.art/
  * Started: 08.03.2022
- * Edited:  09.03.2022
+ * Edited:  10.03.2022
  **/
 
 #include <Arduino.h>
@@ -65,6 +65,55 @@ void theater_chase(uint32_t color, int wait) {
 }
 
 
+/*
+Rainbow cycle along whole strip. Pass delay time (in ms) between frames.
+*/
+void rainbow(int wait) {
+  // Hue of first pixel runs 5 complete loops through the color wheel.
+  // Color wheel has a range of 65536 but it's OK if we roll over, so
+  // just count from 0 to 5*65536. Adding 256 to firstPixelHue each time
+  // means we'll make 5*65536/256 = 1280 passes through this loop:
+  for(long firstPixelHue = 0; firstPixelHue < 5*65536; firstPixelHue += 256) {
+    // pixels.rainbow() can take a single argument (first pixel hue) or
+    // optionally a few extras: number of rainbow repetitions (default 1),
+    // saturation and value (brightness) (both 0-255, similar to the
+    // ColorHSV() function, default 255), and a true/false flag for whether
+    // to apply gamma correction to provide 'truer' colors (default true).
+    pixels.rainbow(firstPixelHue);
+    // Above line is equivalent to:
+    // pixels.rainbow(firstPixelHue, 1, 255, 255, true);
+    pixels.show();
+    delay(wait);
+  }
+}
+
+
+/* 
+Rainbow-enhanced theater marquee. Pass delay time (in ms) between frames.
+*/
+void theaterChaseRainbow(int wait) {
+  int firstPixelHue = 0;     // First pixel starts at red (hue 0)
+
+  for(uint8_t a=0; a<30; a++) {
+    for(uint8_t b=0; b<3; b++) {  //  'b' counts from 0 to 2...
+      pixels.clear();         //   Set all pixels in RAM to 0 (off)
+      // 'c' counts up from 'b' to end of strip in increments of 3...
+      for(uint8_t c=b; c<pixels.numPixels(); c += 3) {
+        // hue of pixel 'c' is offset by an amount to make one full
+        // revolution of the color wheel (range 65536) along the length
+        // of the strip (pixels.numPixels() steps):
+        int      hue   = firstPixelHue + c * 65536L / pixels.numPixels();
+        uint32_t color = pixels.gamma32(pixels.ColorHSV(hue)); // hue -> RGB
+        pixels.setPixelColor(c, color); // Set pixel 'c' to value 'color'
+      }
+      pixels.show();
+      delay(wait);
+      firstPixelHue += 65536 / 90; // One cycle of color wheel over 90 frames
+    }
+  }
+}
+
+
 void boot_blink() {
   uint16_t del_time = 500;
 
@@ -78,31 +127,10 @@ void boot_blink() {
   }
   // Front pixels
   pixels.clear();
+
+  //
+  boot_blink();
 }
-
-
-/*
-Rainbow cycle along whole strip. Pass delay time (in ms) between frames.
-*/
-void rainbow(int wait) {
-  // Hue of first pixel runs 5 complete loops through the color wheel.
-  // Color wheel has a range of 65536 but it's OK if we roll over, so
-  // just count from 0 to 5*65536. Adding 256 to firstPixelHue each time
-  // means we'll make 5*65536/256 = 1280 passes through this loop:
-  for(long firstPixelHue = 0; firstPixelHue < 5*65536; firstPixelHue += 256) {
-    // strip.rainbow() can take a single argument (first pixel hue) or
-    // optionally a few extras: number of rainbow repetitions (default 1),
-    // saturation and value (brightness) (both 0-255, similar to the
-    // ColorHSV() function, default 255), and a true/false flag for whether
-    // to apply gamma correction to provide 'truer' colors (default true).
-    pixels.rainbow(firstPixelHue);
-    // Above line is equivalent to:
-    // strip.rainbow(firstPixelHue, 1, 255, 255, true);
-    pixels.show(); // Update strip with new contents
-    delay(wait);  // Pause for a moment
-  }
-}
-
 
 /*******************************************/
 // 
@@ -149,5 +177,5 @@ void loop() {
 
   rainbow(10);
 
-  
+  //theaterChaseRainbow(50);
 }
